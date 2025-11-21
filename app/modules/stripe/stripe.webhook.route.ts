bd95287d935a1c8348810e4154fb7a3d8cdbe859
dikshit75way <dikshit.7way@gmail.com>
2025-11-21 13:50:32 +0530

    webhook

--- app/modules/stripe/stripe.webhook.route.ts
+++ app/modules/stripe/stripe.webhook.route.ts
@@ -1,7 +1,9 @@
+// backend/routes/stripe.webhook.ts
 import express from "express";
 import bodyParser from "body-parser";
 import { stripe } from "../../common/services/stripe.service";
 import { finalizeBooking } from "../booking/booking.service";
+import { Types } from "mongoose";
 
 const router = express.Router();
 
@@ -10,29 +12,29 @@ router.post(
   bodyParser.raw({ type: "application/json" }),
   async (req, res) => {
     const sig = req.headers["stripe-signature"];
-
     let event;
-
+    console.log("are we hitting on stripe hook")
     try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        sig,
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
-    } catch (err) {
+      event = stripe.webhooks.constructEvent(req.body, sig!, process.env.STRIPE_WEBHOOK_SECRET!);
+    } catch (err: any) {
       return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
-    if (event.type === "payment_intent.succeeded") {
-      const paymentIntent = event.data.object;
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      if(!session) throw new Error("session not found..");
+      if(!session.metadata) throw new Error("meta data not found")
+      if(!session.amount_total) return 
+      console.log("are we hitting the session on backend in webhoook" , session)
 
-      const eventId = paymentIntent.metadata.eventId;
-      const seatIds = JSON.parse(paymentIntent.metadata.seatIds);
-      const userId = paymentIntent.metadata.userId;
-      const amount = paymentIntent.amount / 100;
+      const eventId = session.metadata.eventId;
+      const seatIds = JSON.parse(session.metadata.seatIds);
+      const userId = session.metadata.userId;
+      const amount = session.amount_total / 100;
 
-      // Finalize the booking
-      await finalizeBooking(eventId, seatIds, userId, amount);
+
+   
+     
     }
 
     res.json({ received: true });
